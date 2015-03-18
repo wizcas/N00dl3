102ea23ea65bcbe2c8da1433548c1798594fac21
unknown <792015@HQIT00792015MA.sf.com>
2015-03-18 18:30:51 +0800

    A* project support

---
+++ AStarAI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class AStarAI : MonoBehaviour {
+	public Vector3 targetPosition;
+	public Path path;
+	public float speed = 100f;
+	public float nextWaypointDistance = 3f;
+
+	private Seeker seeker;
+	private CharacterController characterController;
+	private GameController gameController;
+	private int currentWaypoint = 0;
+
+	// Use this for initialization
+	void Start () {
+		seeker = GetComponent<Seeker> ();
+		characterController = GetComponent<CharacterController> ();
+		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
+		gameController.onObstaclePut += Scan;
+		Scan ();
+	}
+
+	void Scan ()
+	{
+		seeker.StartPath (transform.position, targetPosition, OnPathComplete);
+	}
+	
+	void OnPathComplete(Path p)
+	{
+		Debug.Log ("Path back successfully proceeded.");
+		Debug.Log ("Errors? " + p.error);
+		if (!p.error) {
+			path = p;
+			currentWaypoint = 0;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (path == null)
+			return;
+
+		if (currentWaypoint >= path.vectorPath.Count) {
+			Debug.Log ("End of path is reached");
+			return;
+		}
+
+		Vector3 dir = (path.vectorPath [currentWaypoint] - transform.position).normalized;
+		dir *= speed * Time.fixedDeltaTime;
+		characterController.SimpleMove (dir);
+
+		if (Vector3.Distance (transform.position, path.vectorPath [currentWaypoint]) < nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
+	}
+}
---
+++ GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject obstaclePrefab;
+	public delegate void OnObstablePutDelegate();
+	public event OnObstablePutDelegate onObstaclePut;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			PutObstacle();
+		}
+	}
+
+	void PutObstacle()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 50f, LayerMask.GetMask ("Ground"))) {
+			Vector3 pos = hit.point + new Vector3(0f, 2f, 0f);
+			GameObject obj = Instantiate(obstaclePrefab, pos, Quaternion.identity) as GameObject;
+			AstarPath.active.UpdateGraphs (obj.collider.bounds);
+			if(onObstaclePut != null)
+				onObstaclePut();
+		}
+	}
+}
